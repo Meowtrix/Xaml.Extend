f5f9735fab432e897ee8f28fe603351c9bbc0412
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2016-07-31 23:45:45 +0800

    FilePicker功能

--- Meowtrix.WPF.Extend/Controls/FilePicker.cs
+++ Meowtrix.WPF.Extend/Controls/FilePicker.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
+using Microsoft.WindowsAPICodePack.Dialogs;
 
 namespace Meowtrix.WPF.Extend.Controls
 {
+    public enum FilePickerType { OpenFile, SaveFile, Folder }
+
     public class FilePicker : Control
     {
         static FilePicker()
@@ -22,5 +27,45 @@ namespace Meowtrix.WPF.Extend.Controls
             DependencyProperty.Register("Filename", typeof(string), typeof(FilePicker), new PropertyMetadata(string.Empty));
 
         public event EventHandler<PropertyChangedEventArgs<string>> FilenameChanged;
+
+        public FilePickerType PickerType
+        {
+            get { return (FilePickerType)GetValue(PickerTypeProperty); }
+            set { SetValue(PickerTypeProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for PickerType.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty PickerTypeProperty =
+            DependencyProperty.Register("PickerType", typeof(FilePickerType), typeof(FilePicker), new PropertyMetadata(FilePickerType.OpenFile));
+
+        public IEnumerable<string> Filters
+        {
+            get { return (IEnumerable<string>)GetValue(FiltersProperty); }
+            set { SetValue(FiltersProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Filters.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty FiltersProperty =
+            DependencyProperty.Register("Filters", typeof(IEnumerable<string>), typeof(FilePicker), new PropertyMetadata(null));
+
+        private void OnClick(object sender, RoutedEventArgs e)
+        {
+            CommonFileDialog dialog;
+            if (PickerType == FilePickerType.OpenFile) dialog = new CommonOpenFileDialog();
+            else if (PickerType == FilePickerType.SaveFile) dialog = new CommonSaveFileDialog();
+            else dialog = new CommonOpenFileDialog { IsFolderPicker = true };
+            if (Filters != null)
+                foreach (var str in Filters)
+                    dialog.Filters.Add(new CommonFileDialogFilter(str, str));
+            dialog.DefaultDirectory = Path.GetDirectoryName(Filename);
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                var old = Filename;
+                Filename = dialog.FileName;
+                var temp = FilenameChanged;
+                temp?.Invoke(this, new PropertyChangedEventArgs<string>(old, Filename));
+            }
+            dialog.Dispose();
+        }
     }
 }
