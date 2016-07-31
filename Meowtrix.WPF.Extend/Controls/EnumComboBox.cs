3bf7018808620058f6183ab36961a48956852f27
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2016-07-31 20:10:04 +0800

    创建EnumComboBox类

---
+++ Meowtrix.WPF.Extend/Controls/EnumComboBox.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Controls;
+
+namespace Meowtrix.WPF.Extend.Controls
+{
+    public class EnumComboBox : ComboBox
+    {
+        private Type _enumtype;
+        public Type EnumType
+        {
+            get { return _enumtype; }
+            set
+            {
+                if (!value.IsEnum) throw new ArgumentException(nameof(EnumType));
+                _enumtype = value;
+                BuildItemsSource(value);
+            }
+        }
+
+        private void BuildItemsSource(Type type)
+        {
+
+        }
+    }
+}
