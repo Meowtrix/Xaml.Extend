8d27024ce57ade36e21680d46b1a00e4a814a246
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2016-08-01 15:32:02 +0800

    v1.1.1

--- Meowtrix.WPF.Extend/Properties/AssemblyInfo.cs
+++ Meowtrix.WPF.Extend/Properties/AssemblyInfo.cs
@@ -52,7 +52,7 @@ using System.Windows.Markup;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.1.1.0")]
+[assembly: AssemblyFileVersion("1.1.1.0")]
 [assembly: XmlnsDefinition("urn:meowtrix", "Meowtrix.WPF.Extend")]
 [assembly: XmlnsDefinition("urn:meowtrix", "Meowtrix.WPF.Extend.Controls")]
