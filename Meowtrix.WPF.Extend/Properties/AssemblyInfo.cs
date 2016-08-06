a4f60e46603af70239260bbf93b23289a4352285
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2016-08-06 14:20:06 +0800

    ThemeStyle

--- Meowtrix.WPF.Extend/Properties/AssemblyInfo.cs
+++ Meowtrix.WPF.Extend/Properties/AssemblyInfo.cs
@@ -33,7 +33,7 @@ using System.Windows.Markup;
 
 
 [assembly:ThemeInfo(
-    ResourceDictionaryLocation.None, //主题特定资源词典所处位置
+    ResourceDictionaryLocation.SourceAssembly, //主题特定资源词典所处位置
                              //(当资源未在页面
                              //或应用程序资源字典中找到时使用)
     ResourceDictionaryLocation.SourceAssembly //常规资源词典所处位置
