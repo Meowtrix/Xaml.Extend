27cd862f0c3255c38bf47ca3de447fb6e46be676
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2016-08-06 14:22:44 +0800

    CommandHyperlink

---
+++ Meowtrix.WPF.Extend/Controls/CommandHyperlink.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Documents;
+
+namespace Meowtrix.WPF.Extend.Controls
+{
+    public class CommandHyperlink : Hyperlink
+    {
+        public string CommandLineString
+        {
+            get { return (string)GetValue(CommandLineStringProperty); }
+            set { SetValue(CommandLineStringProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for CommandLineString.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty CommandLineStringProperty =
+            DependencyProperty.Register(nameof(CommandLineString), typeof(string), typeof(CommandHyperlink), new PropertyMetadata(null));
+
+        protected override void OnClick()
+        {
+            if (CommandLineString != null)
+                System.Diagnostics.Process.Start(CommandLineString);
+        }
+    }
+}
