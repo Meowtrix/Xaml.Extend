3dc5f47fba469f00fccb5adf375150e884198ff8
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2016-08-22 18:31:45 +0800

    DynamicBaseStyle

--- Meowtrix.WPF.Extend/StyleBehavior.cs
+++ Meowtrix.WPF.Extend/StyleBehavior.cs
@@ -20,5 +20,56 @@ namespace Meowtrix.WPF.Extend
             newstyle.Seal();
             return newstyle;
         }
+
+        public static object GetDynamicBaseStyle(FrameworkElement obj) => (object)obj.GetValue(DynamicBaseStyleProperty);
+
+        public static void SetDynamicBaseStyle(FrameworkElement obj, object value) => obj.SetValue(DynamicBaseStyleProperty, value);
+
+        // Using a DependencyProperty as the backing store for DynamicBaseStyle.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty DynamicBaseStyleProperty =
+            DependencyProperty.RegisterAttached("DynamicBaseStyle", typeof(object), typeof(StyleBehavior), new PropertyMetadata(null, OnDynamicBaseStyleChanged));
+
+        private static void OnDynamicBaseStyleChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (e.OldValue == e.NewValue) return;
+            var c = d as FrameworkElement;
+            if (c == null) throw new NotSupportedException("DynamicBaseStyle is only available on FrameworkElement");
+            var key = e.NewValue;
+            if (key != null)
+                c.SetResourceReference(BasedOnStyleProperty, key);
+            else
+                c.ClearValue(BasedOnStyleProperty);
+        }
+
+        public static Style GetBasedOnStyle(FrameworkElement obj) => (Style)obj.GetValue(BasedOnStyleProperty);
+
+        public static void SetBasedOnStyle(FrameworkElement obj, Style value) => obj.SetValue(BasedOnStyleProperty, value);
+
+        // Using a DependencyProperty as the backing store for BasedOnStyle.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty BasedOnStyleProperty =
+            DependencyProperty.RegisterAttached("BasedOnStyle", typeof(Style), typeof(StyleBehavior), new PropertyMetadata(null, OnBasedOnStyleChanged));
+
+        private static void OnBasedOnStyleChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (e.OldValue == e.NewValue) return;
+            FrameworkElement c = d as FrameworkElement;
+            if (c == null) throw new NotSupportedException("DynamicBaseStyle is only available on FrameworkElement");
+            var baseStyle = e.NewValue as Style;
+            var originalStyle = GetOriginalStyle(c);
+            if (originalStyle == null)
+            {
+                originalStyle = c.Style;
+                SetOriginalStyle(c, originalStyle);
+            }
+            c.Style = Merge(baseStyle, originalStyle);
+        }
+
+        public static Style GetOriginalStyle(FrameworkElement obj) => (Style)obj.GetValue(OriginalStyleProperty);
+
+        public static void SetOriginalStyle(FrameworkElement obj, Style value) => obj.SetValue(OriginalStyleProperty, value);
+
+        // Using a DependencyProperty as the backing store for OriginalStyle.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty OriginalStyleProperty =
+            DependencyProperty.RegisterAttached("OriginalStyle", typeof(Style), typeof(StyleBehavior), new PropertyMetadata(null));
     }
 }
